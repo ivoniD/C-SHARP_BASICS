0d03cba6e1c540ebd0b7a587c89ad562f92969e1
Ivon Dimitrova <117980916+ivoniD@users.noreply.github.com>
2022-12-13 18:40:02 +0200

    Update Program.cs

--- 08-Pet Shop/Program.cs
+++ 08-Pet Shop/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace zadacha_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int dogFood = int.Parse(Console.ReadLine());
+            int catFood = int.Parse(Console.ReadLine());
+
+            double foodCost = (dogFood * 2.50) + (catFood * 4);
+
+
+            Console.WriteLine($"{foodCost} lv.");
+
+
+        }
+    }
+}
