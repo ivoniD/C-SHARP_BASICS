435cb923bdcec440fbb70a32e16e594d49a30e6b
Ivon Dimitrova <117980916+ivoniD@users.noreply.github.com>
2022-12-13 18:29:47 +0200

    Update Program.cs

--- 02-Nums 1-101/Program.cs
+++ 02-Nums 1-101/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace zadacha_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(1);
+            Console.WriteLine(2);
+            Console.WriteLine(3);
+            Console.WriteLine(4);
+            Console.WriteLine(5);
+            Console.WriteLine(6);
+            Console.WriteLine(7);
+            Console.WriteLine(8);
+            Console.WriteLine(9);
+            Console.WriteLine(10);
+
+        }
+    }
+}
