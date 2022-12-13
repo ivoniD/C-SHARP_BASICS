0f781772b0860dd7c41f3190ea84f06e4e3888a0
Ivon Dimitrova <117980916+ivoniD@users.noreply.github.com>
2022-12-13 18:40:31 +0200

    Update Program.cs

--- 09-Yard Greening/Program.cs
+++ 09-Yard Greening/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace zadacha_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double landscapingArea = double.Parse(Console.ReadLine());
+
+            double Cost = landscapingArea * 7.61;
+
+            double discount = Cost * 0.18;
+
+            double totalCost = Cost - discount;
+
+            Console.WriteLine($"The final price is: {totalCost} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+
+
+
+        }
+    }
+}
