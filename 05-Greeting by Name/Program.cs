f33e6e6c8e6e4f971bfcf99ca646e82aac4d0709
Ivon Dimitrova <117980916+ivoniD@users.noreply.github.com>
2022-12-13 18:33:09 +0200

    Update Program.cs

--- 05-Greeting by Name/Program.cs
+++ 05-Greeting by Name/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"Hello, {name}!");
+
+    }
+}
