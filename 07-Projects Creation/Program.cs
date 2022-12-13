4d0c85ef70ce95253dd1deb40d049ba7e28860fa
Ivon Dimitrova <117980916+ivoniD@users.noreply.github.com>
2022-12-13 18:34:07 +0200

    Update Program.cs

--- 07-Projects Creation/Program.cs
+++ 07-Projects Creation/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace zadacha_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+            int number = int.Parse(Console.ReadLine());
+            int hours = number * 3;
+
+
+            Console.WriteLine($"The architect {name} will need {hours} hours to complete {number} project/s.");
+
+
+        }
+    }
+}
